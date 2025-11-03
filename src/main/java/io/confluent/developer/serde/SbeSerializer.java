a4a33284bff541c0cb98b9805d665284c4ca30a9
bbejeck <bbejeck@gmail.com>
2025-11-02 22:16:23 -0500

    Refactor SBE serializer to handle null encoder gracefully and improve byte array extraction logic; update build script with additional SBE generation properties.

--- src/main/java/io/confluent/developer/serde/SbeSerializer.java
+++ src/main/java/io/confluent/developer/serde/SbeSerializer.java
@@ -3,13 +3,24 @@ package io.confluent.developer.serde;
 import baseline.StockTradeEncoder;
 import org.apache.kafka.common.serialization.Serializer;
 
+import java.nio.ByteBuffer;
+
 
 public class SbeSerializer implements Serializer<StockTradeEncoder> {
 
     @Override
     public byte[] serialize(String s, StockTradeEncoder stockTradeEncoder) {
+        if (stockTradeEncoder == null) {
+            return null;
+        }
+        ByteBuffer byteBuffer = stockTradeEncoder.buffer().byteBuffer();
+
+        if (byteBuffer.hasArray() && byteBuffer.array().length == stockTradeEncoder.limit()) {
+            return byteBuffer.array();
+        }
+        byteBuffer.rewind();
         byte[] array = new byte[stockTradeEncoder.limit()];
-        stockTradeEncoder.buffer().byteBuffer().get(array, 0, stockTradeEncoder.limit());
+        byteBuffer.get(array, 0, stockTradeEncoder.limit());
         return array;
     }
 }
